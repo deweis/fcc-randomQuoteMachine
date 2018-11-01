b5ea56119e99339d38c1b399d2ed823815ef7d97
Dennis <deweis@gmx.ch>
2018-11-01 12:13:40 +0100

    Add the Quote Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
+import Quote from './Quote/Quote';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>Quote Generator</h1>
+        <Quote />
       </div>
     );
   }
---
+++ src/Quote/Quote.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const quote = () => {
+  return <p>May you live every day of your life!</p>;
+};
+
+export default quote;
